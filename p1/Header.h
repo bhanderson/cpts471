9e0b2d3bf0f7e7e87abff4e7afce2248c6fc18e8
Bryce Handerson <bryce.handerson@wsu.edu>
2014-02-20 13:16:13 -0800

    input strings working

--- p1/Header.h
+++ p1/Header.h
@@ -4,11 +4,13 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAXSTRLEN 128
+#define MAXSTRLEN 256
 
-typedef enum { false, true } bool;
+int setting = 0, ma = 0, mi = 0, g = 0, h = 0;
+char s1[MAXSTRLEN], s2[MAXSTRLEN];
+char s1name[64], s2name[64];
 
-typedef struct {
+typedef struct DP_cell{
 	int score;
 	char dir;
 } DP_cell;
@@ -27,7 +29,7 @@ void inputerror(){
 int strtoint(char *str){
 	int i = 0, val = 0, result = 0, temp = 0;
 
-	printf("converting %s\n", str);
+	//printf("converting %s\n", str);
 
 	if(str[0] == '-'){
 		for (i=1; str[i] <= '9' && str[i] >= '0'; i++) {
@@ -61,4 +63,125 @@ int init()
 	}
 
 }
+
+int settings(const char *argv[]){
+	char sline[32];
+	char temp;
+	char strpath[64];
+	char configpath[64];
+
+	if(argv[1] && argv[2] && (*argv[2] == '0' || *argv[2] == '1')){
+		strncpy(strpath, argv[1], 64);
+		strncpy(&temp, argv[2], 1);
+		setting = temp - '0';
+	}else{
+		goto ERROR;
+	}
+
+	if(strinput(strpath)== -1){
+		return -1;
+	}
+
+	if(argv[3]){
+		strncpy(configpath, argv[3], 64);
+	}
+
+	printf("Local or Dynamic: ");
+	if (setting == 0) {
+		printf("GLOBAL\n");
+	} else if (setting = 1) {
+		printf("LOCAL\n");
+	} else
+		printf("UNKNOWN\n");
+	//printf("Config: %s\n", configpath);
+
+	FILE *fp = fopen(configpath, "r");
+
+	if (fp) {
+		while(fgets(sline, 32, fp)){
+			char *tok = strtok(sline, " \t\n");
+			if(strcmp(tok, "match") == 0){
+				tok = strtok(NULL, "\t\n");
+				//printf("%s",tok);
+				ma = strtoint(tok);
+				printf("Match score: %d\n", ma);
+			}
+			else if(strcmp(tok, "mismatch") == 0){
+				tok = strtok(NULL, " \t\n");
+				mi = strtoint(tok);
+				printf("Mismatch score: %d\n", mi);
+				//printf("%s\n", strtok(NULL, " \t\n"));
+
+			}
+			else if(strcmp(tok, "h") == 0){
+				tok = strtok(NULL, " \t\n");
+				h = strtoint(tok);
+				printf("h score: %d\n", h);
+			}
+			else if(strcmp(tok, "g") == 0){
+				tok = strtok(NULL, " \t\n");
+				g = strtoint(tok);
+				printf("g score: %d\n", g);
+			}
+
+		}
+	}
+
+	fclose(fp);
+	return 0;
+
+ERROR:
+	if(fp)
+		fclose(fp);
+	printf("TEST\n");
+	inputerror();
+	return -1;
+}
+
+int strinput(char *path){
+	//printf("strings: %s\n", strpath);
+	FILE *sfp = fopen(path, "r");
+	int c, n = 0, strnum = 0;
+	char line[256];
+
+	if (sfp){
+		//printf("1\n");
+		while ((c = fgetc(sfp)) != EOF && n < MAXSTRLEN) {
+			switch(c){
+				//printf("C: %c\n", c);
+				case 'a':
+				case 'c':
+				case 't':
+				case 'g':
+					if(strnum == 1)
+						s1[n++] = c;
+					else
+						s2[n++] = c;
+					break;
+				case '>':
+					strnum++;
+					n = 0;
+					fgets(line, sizeof(line), sfp);
+					char *tok = strtok(line, " ");
+					if (strnum == 1) {
+						strcpy(s1name, tok);
+					} else if (strnum == 2) {
+						strcpy(s2name, tok);
+					}
+					break;
+			}
+		}
+		if(n == MAXSTRLEN){
+			printf("ERROR SEQUENCE %d SIZE TOO LARGE\n", strnum);
+			if(sfp)
+				fclose(sfp);
+			return -1;
+		}
+		printf("%s: %s\n", s1name, s1);
+		printf("%s: %s\n", s2name, s2);
+	}
+	fclose(sfp);
+	return 0;
+}
+
 #endif
--- p1/Main.c
+++ p1/Main.c
@@ -2,82 +2,27 @@
 
 int main(int argc, const char *argv[])
 {
+	char linein[256];
 	char input;
 	char one[MAXSTRLEN], two[MAXSTRLEN];
-	char strpath[64], configpath[64];
-	char temp[1];
-	bool setting;
-	char line[32];
-	int ma, mi, g, h;
 
-	if(argv[1] && argv[2] && (*argv[2] == '0' || *argv[2] == '1')){
-		strncpy(strpath, argv[1], 64);
-		strncpy(temp, argv[2], 1);
-		setting = *temp - '0';
-	}else{
-		inputerror();
-		return 0;
+	if(settings(argv)){
+		return -1;
 	}
-
-	if(argv[3]){
-		strncpy(configpath, argv[3], 64);
-	}
-
-	printf("strings: %s\n", strpath);
-	printf("Local or Dynamic: %d\n", setting);
-	printf("Config: %s\n", configpath);
-
-	FILE *fp;
-	fp = fopen(configpath, "r");
-
-	if (fp) {
-		while(fgets(line, 32, fp)){
-			char *tok = strtok(line, " \t\n");
-			if(strcmp(tok, "match") == 0){
-				tok = strtok(NULL, "\t\n");
-				printf("%s",tok);
-				ma = strtoint(tok);
-				printf("Match score: %d\n", ma);
-			}
-			else if(strcmp(tok, "mismatch") == 0){
-				tok = strtok(NULL, " \t\n");
-				printf("Mismatch score: %d\n", strtoint(tok));
-				//printf("%s\n", strtok(NULL, " \t\n"));
-
-			}
-			else if(strcmp(tok, "h") == 0){
-
-			}
-			else if(strcmp(tok, "g") == 0){
-
-			}
-			
-		}
-		/*
-		   while(fgets(line, 32, fp)){
-		   char *tok = strtok(line, " \t\n");
-		   while(tok!= NULL){
-		   printf("%s",tok);
-		   tok = (NULL, " \t\n");
-		   }
-		   printf("a\n");
-		   fgets(line, 32, fp);
-		   }
-		   */
-	}
-
 	while (1) {
 		print_menu();
 		int i,j;
 		init();
-		input = getchar();
-		getchar(); // discard newline char
+		if(fgets(linein, sizeof(linein), stdin))
+			input = linein[0];
 		switch(input){
 			case 'n':
 			case 'N':
+				printf("Needleman-Wunsch:\n");
 				break;
 			case 's':
 			case 'S':
+				printf("Smith-Waterman:\n");
 				break;
 			case 'q':
 			case 'Q':
@@ -85,8 +30,8 @@ int main(int argc, const char *argv[])
 				break;
 			default:
 				printf("Error invalid command\n");
+				break;
 		}
 	}
-
 	return 0;
 }
