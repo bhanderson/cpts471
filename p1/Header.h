7c5015ce5d7007630e8edde861e14f3a355fa07d
Bryce Handerson <bryce.handerson@wsu.edu>
2014-02-21 23:38:37 -0800

    working i think

--- p1/Header.h
+++ p1/Header.h
@@ -10,9 +10,10 @@
 #define DEL 2
 #define INS 4
 
-int setting = 0, ma = 0, mi = 0, g = 0, h = 0;
+int local = 0, ma = 0, mi = 0, g = 0, h = 0;
 char s1[MAXSTRLEN], s2[MAXSTRLEN];
 char s1name[64], s2name[64];
+int highscore[2];
 
 typedef struct DP_cell{
 	int score;
@@ -27,7 +28,48 @@ void print_menu(){
 }
 
 
-void revstring(char *str){
+void wordwrap(char* a1, char* m, char* a2){
+	int count1 = 1, count2 = 1;
+	int i;
+	int apos = 0, bpos = 0, mpos = 0;
+	int j= 0;
+	while(a2[bpos] != 0 && a1[apos] != 0){
+		i = 0;
+		printf("s1: %d\t", count1);
+		for (i = 0; i < 60 && a1[apos] != 0; i++) {
+			printf("%c", a1[apos]);
+			if (a1[apos] == 'a' || a1[apos] == 'c' || a1[apos] == 'g' || a1[apos] == 't' || a1[apos] == 'A' || a1[apos] == 'C' || a1[apos] == 'G' || a1[apos] == 'T') {
+				count1++;
+			}
+			apos++;
+		}
+		printf(" %d", count1-1);
+		printf("\n");
+		printf("      \t");
+		i = 0;
+		for (i = 0; i < 60 && m[mpos] != 0; i++) {
+			printf("%c", m[mpos]);
+			mpos++;
+		}
+		printf("\n");
+
+		printf("s2: %d\t", count2);
+		i = 0;
+		for (i = 0; i < 60 && a2[bpos] != 0; i++) {
+			printf("%c", a2[bpos]);
+			if (a2[bpos] == 'a' || a2[bpos] == 'c' || a2[bpos] == 'g' || a2[bpos] == 't' || a2[bpos] == 'A' || a2[bpos] == 'C' || a2[bpos] == 'G' || a2[bpos] == 'T') {
+				count2++;
+			}
+			bpos++;
+		}
+		printf(" %d", count2-1);
+		printf("\n");
+		j++;
+	}
+	return;
+}
+
+char* revstring(char *str){
 	int end = strlen(str)-1;
 	int start = 0;
 
@@ -36,10 +78,10 @@ void revstring(char *str){
 		str[start] ^= str[end];
 		str[end] ^= str[start];
 		str[start]^= str[end];
-
 		++start;
 		--end;
 	}
+	return str;
 }
 
 
@@ -113,10 +155,16 @@ int align(char *s1, char *s2){
 	for (i = 1; i < MAXSTRLEN; i++) { // initialize the first column and row
 		array[i][0].score = h + (g * (i-1));
 		array[i][0].dir = DEL;
+		if (local) {
+			array[i][0].score = 0;
+		}
 	}
 	for (j = 1; j < MAXSTRLEN; j++) {
 		array[0][j].score = h + (g * (j-1));
 		array[0][j].dir = INS;
+		if (local) {
+			array[0][j].score = 0;
+		}
 	}
 
 	for (i = 1; i < m+1; i++) {
@@ -139,6 +187,13 @@ int align(char *s1, char *s2){
 
 			// set the score and the direction the score was found to the cell
 			array[i][j].score = max(su, de, in, &dir);
+			if (array[i][j].score > array[highscore[0]][highscore[1]].score) {
+				highscore[0] = i;
+				highscore[1] = j;
+			}
+			if (local && array[i][j].score < 0) {
+				array[i][j].score = 0;
+			}
 			array[i][j].dir = dir;
 		}
 	}
@@ -155,7 +210,7 @@ int settings(const char *argv[]){
 	if(argv[1] && argv[2] && (*argv[2] == '0' || *argv[2] == '1')){
 		strncpy(strpath, argv[1], 64);
 		strncpy(&temp, argv[2], 1);
-		setting = temp - '0';
+		local = temp - '0';
 	}else{
 		goto ERROR;
 	}
@@ -169,9 +224,9 @@ int settings(const char *argv[]){
 	}
 
 	printf("Local or Dynamic: ");
-	if (setting == 0) {
+	if (!local) {
 		printf("GLOBAL\n");
-	} else if (setting = 1) {
+	} else if (local) {
 		printf("LOCAL\n");
 	} else
 		printf("UNKNOWN\n");
@@ -270,39 +325,81 @@ int stringsinput(char *path){
 	return 0;
 }
 
+int localretrace(int i, int j){ // input the position of where to start
+	char revs1[MAXSTRLEN*2], revs2[MAXSTRLEN*2], match[MAXSTRLEN*2];
+	revs1[0] = revs2[0] = match[0] = 0;
+	int count = 0, dir=0;
+	// while still in the array and not the last place on the alignment
+	printf("Start score: %d\n", array[i][j]);
+	printf("i: %d j: %d", i, j);
+	printf("S1: %s\n", s1);
+	printf("S2: %s\n", s2);
+	while(array[i][j].score > 0){
+		max(array[i-1][j-1].score, array[i-1][j].score, array[i][j-1].score, &dir);
+		if(dir & SUB){ // substitution
+			revs1[count] = s1[i-1];
+			revs2[count] = s2[j-1];
+			if (s1[i-1] == s2[j-1]) {
+				strcat(match, "|");
+			} else
+				strcat(match, " ");
+			i--; j--;
+		} else if (dir & DEL) { // deletion
+			revs1[count] = s1[i-1];
+			revs2[count] = '-';
+			strcat(match, " ");
+			i--;
+		} else if (dir & INS) {
+			revs1[count] = '-';
+			revs2[count] = s2[j-1];
+			strcat(match, " ");
+			j--;
+		}
+		count++;
+	}
+	//strcat(match, "?");
+	revs1[count] = 0;
+	revs2[count] = 0;
+	printf("%s\n", revstring(revs1));
+	printf("%s\n", revstring(match));
+	printf("%s\n", revstring(revs2));
+}
+
+
 
 int retrace(){
-	int i = strlen(s1)-1, j = strlen(s2)-1;
+	int i = strlen(s1), j = strlen(s2);
 	int dir = 0;
 	int count = 0;
 	int gaps = 0, matches = 0, mismatches = 0;
 	char res1[MAXSTRLEN], res2[MAXSTRLEN], match[MAXSTRLEN];
+	match[0]=0;
 
 	while(i !=0 && j !=0){
 		max(array[i-1][j-1].score, array[i-1][j].score, array[i][j-1].score, &dir);
 		if(dir & SUB){
-			res1[count] = s1[i];
-			res2[count] = s2[j];
-			if(s1[i]==s2[j]){
+			res1[count] = s1[i-1];
+			res2[count] = s2[j-1];
+			if(s1[i-1]==s2[j-1]){
 				matches++;
-				match[count] = '|';
+				strcat(match, "|");
 			}
 			else{
 				mismatches++;
-				match[count] = ' ';
+				strcat(match, " ");
 			}
 			i--;
 			j--;
 		} else if(dir & DEL){
-			res1[count] = s1[i];
+			res1[count] = s1[i-1];
 			res2[count] = '-';
-			match[count] = ' ';
+			strcat(match, " ");
 			gaps++;
 			i--;
 		}else if(dir & INS){
 			res1[count] = '-';
-			res2[count] = s2[j];
-			match[count] = ' ';
+			res2[count] = s2[j-1];
+			strcat(match, " ");
 			j--;
 		}
 		count++;
@@ -314,8 +411,8 @@ int retrace(){
 	revstring(match);
 	revstring(res2);
 	printf("%s\n", res1);
-	printf("%s\n", match);
 	printf("%s\n", res2);
+	wordwrap(res1, match, res2);
 
 }
 
--- p1/Main.c
+++ p1/Main.c
@@ -8,32 +8,11 @@ int main(int argc, const char *argv[])
 	if(settings(argv)){
 		return -1;
 	}
-	while (1) {
-		print_menu();
-		if(fgets(linein, sizeof(linein), stdin))
-			input = linein[0];
-		switch(input){
-			case 'n':
-			case 'N':
-				printf("Needleman-Wunsch:\n");
-				align(s1,s2);
-				//printf("%d", array[strlen(s1)][strlen(s2)]);
-				//printarray();
-				printf("SCORE: %d\n", array[strlen(s1)][strlen(s2)].score);
-				retrace();
-				break;
-			case 's':
-			case 'S':
-				printf("Smith-Waterman:\n");
-				break;
-			case 'q':
-			case 'Q':
-				return 0;
-				break;
-			default:
-				printf("Error invalid command\n");
-				break;
-		}
-	}
+	align(s1,s2);
+//	printarray();
+	if(local)
+		localretrace(highscore[0],highscore[1]);
+	else
+		retrace();
 	return 0;
 }
