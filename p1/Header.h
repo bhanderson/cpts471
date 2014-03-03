07e0e259ed0efd25caf158901a6d3915224f1369
Bryce Handerson <bryce.handerson@gmail.com>
2014-03-03 14:29:58 -0800

    checking score

--- p1/Header.h
+++ p1/Header.h
@@ -15,9 +15,10 @@
 int local = 0, ma = 0, mi = 0, g = 0, h = 0;
 //char s1[MAXSTRLEN], s2[MAXSTRLEN];
 char *dynamicstring;
-char s1name[64], s2name[64];
+char *s1, *s2, *s1name, *s2name;
+//char s1name[64], s2name[64];
 int highscore[2];
-int strnum;
+int strnum = 0;
 
 
 typedef struct DP_cell{
@@ -42,7 +43,7 @@ void wordwrap(char* a1, char* m, char* a2){
 	int j= 0;
 	while(a2[bpos] != 0 && a1[apos] != 0){
 		i = 0;
-		printf("s1: %d\t", count1);
+		printf("s1: %05d\t", count1);
 		for (i = 0; i < 60 && a1[apos] != 0; i++) {
 			printf("%c", a1[apos]);
 			if (a1[apos] == 'a' || a1[apos] == 'c' || a1[apos] == 'g' || a1[apos] == 't' || a1[apos] == 'A' || a1[apos] == 'C' || a1[apos] == 'G' || a1[apos] == 'T') {
@@ -52,7 +53,7 @@ void wordwrap(char* a1, char* m, char* a2){
 		}
 		printf(" %d", count1-1);
 		printf("\n");
-		printf("      \t");
+		printf("\t\t");
 		i = 0;
 		for (i = 0; i < 60 && m[mpos] != 0; i++) {
 			printf("%c", m[mpos]);
@@ -60,7 +61,7 @@ void wordwrap(char* a1, char* m, char* a2){
 		}
 		printf("\n");
 
-		printf("s2: %d\t", count2);
+		printf("s2: %05d\t", count2);
 		i = 0;
 		for (i = 0; i < 60 && a2[bpos] != 0; i++) {
 			printf("%c", a2[bpos]);
@@ -153,25 +154,45 @@ int strtoint(char *str){
 	return result;
 }
 
-/*
+int allarr(int m, int n){
+	DP_cell **temp;
+	temp = malloc(m * sizeof(DP_cell*));
+	if (temp == 0){
+		printf("Error allocatign dynamic table");
+		return -1;
+	}
+	for (int i = 0; i < m; i++){
+		temp[i] = malloc(n * sizeof(DP_cell));
+		if(temp[i] == NULL){
+			printf("Error allocating dynamic table");
+			return -1;
+		}
+	}
+	dynamicarray = temp;
+	return 0;
+}
+
+
 int align(char *s1, char *s2){
 	int i,j,m,n, dir = 0;
 	int su, de, in;
 	m = strlen(s1);
 	n = strlen(s2);
-	array[0][0].score = 0;
-	for (i = 1; i < MAXSTRLEN; i++) { // initialize the first column and row
-		array[i][0].score = h + (g * (i-1));
-		array[i][0].dir = DEL;
+	if(allarr(m+1,n+1)<0)
+		return -1;
+	dynamicarray[0][0].score = 0;
+	for (i = 1; i < m+1; i++) { // initialize the first column and row
+		dynamicarray[i][0].score = h + (g * (i-1));
+		dynamicarray[i][0].dir = DEL;
 		if (local) {
-			array[i][0].score = 0;
+			dynamicarray[i][0].score = 0;
 		}
 	}
-	for (j = 1; j < MAXSTRLEN; j++) {
-		array[0][j].score = h + (g * (j-1));
-		array[0][j].dir = INS;
+	for (j = 1; j < n+1; j++) {
+		dynamicarray[0][j].score = h + (g * (j-1));
+		dynamicarray[0][j].dir = INS;
 		if (local) {
-			array[0][j].score = 0;
+			dynamicarray[0][j].score = 0;
 		}
 	}
 
@@ -179,40 +200,40 @@ int align(char *s1, char *s2){
 		for (j = 1; j < n+1; j++) {
 			// substitution
 			if (strncmp(&s1[i-1], &s2[j-1], 1)==0)
-				su = array[i-1][j-1].score + ma;
+				su = dynamicarray[i-1][j-1].score + ma;
 			else
-				su = array[i-1][j-1].score + mi;
+				su = dynamicarray[i-1][j-1].score + mi;
 			// deletion
-			if(array[i-1][j].dir & DEL)
-				de = array[i-1][j].score + g;
+			if(dynamicarray[i-1][j].dir & DEL)
+				de = dynamicarray[i-1][j].score + g;
 			else
-				de = array[i-1][j].score + h + g;
+				de = dynamicarray[i-1][j].score + h + g;
 			// insertion
-			if(array[i][j-1].dir & INS)
-				in = array[i][j-1].score + g;
+			if(dynamicarray[i][j-1].dir & INS)
+				in = dynamicarray[i][j-1].score + g;
 			else
-				in = array[i][j-1].score + h + g;
+				in = dynamicarray[i][j-1].score + h + g;
 
 			// set the score and the direction the score was found to the cell
-			array[i][j].score = max(su, de, in, &dir);
-			if (array[i][j].score > array[highscore[0]][highscore[1]].score) {
+			dynamicarray[i][j].score = max(su, de, in, &dir);
+			if (dynamicarray[i][j].score > dynamicarray[highscore[0]][highscore[1]].score) {
 				highscore[0] = i;
 				highscore[1] = j;
 			}
-			if (local && array[i][j].score < 0) {
-				array[i][j].score = 0;
+			if (local && dynamicarray[i][j].score < 0) {
+				dynamicarray[i][j].score = 0;
 			}
-			array[i][j].dir = dir;
+			dynamicarray[i][j].dir = dir;
 		}
 	}
 	if (local)
-		printf("Optimal Score: %d\n", array[highscore[0]][highscore[1]].score);
+		printf("Optimal Score: %d\n", dynamicarray[highscore[0]][highscore[1]].score);
 
 	else
-		printf("Optimal Score: %d\n", array[i-1][j-1].score);
+		printf("Optimal Score: %d\n", dynamicarray[i-1][j-1].score);
 	return 0;
 }
-*/
+
 /*
 int stringsinput(char *path){
 	FILE *sfp = fopen(path, "r");
@@ -272,14 +293,17 @@ int dynamicstrinput(char *path){
 
 	if (stat(path, &st) < 0){
 		printf("Error reading file\n");
-		return 1;
+		return -1;
 	}
 	size = st.st_size;
 	printf("Path: %s\n", path);
 	printf("FILESIZE: %d\n", size);
 	dynamicstring = (char *)malloc(size * sizeof(char));
-	pos = dynamicstring;
 
+	if (dynamicstring == 0)
+		return -1;
+
+	pos = dynamicstring;
 	FILE *dsfp = fopen(path, "r");
 	if (!dsfp){
 		return -1;
@@ -287,17 +311,28 @@ int dynamicstrinput(char *path){
 		while((c = fgetc(dsfp)) != EOF){
 			switch(c){
 				case '>':
-					if (*pos){ // there is a letter here
-						pos++;
+					if (strnum == 0) // it is the first string
+						s1name = pos;
+					else{
 						*pos = 0;
-						pos++;
+						pos ++;
+						s2name = pos;
 					}
+					// get the line of the header
+					// then copy the line to the dynamic array
+					// terminate it with zero then move the pointer to the next
+					// char position
 					fgets(line, sizeof(line), dsfp);
 					char *tok = strtok(line, " ");
 					strcpy(pos, tok);
 					pos += strlen(tok);
 					*pos = 0;
 					pos++;
+					if (strnum == 0){
+						s1 = pos;
+					} else
+						s2 = pos;
+					strnum++;
 					break;
 				case 'a':
 				case 'A':
@@ -307,15 +342,18 @@ int dynamicstrinput(char *path){
 				case 'G':
 				case 't':
 				case 'T':
-						pos++;
 						*pos = c;
+						pos++;
+						break;
 				default:
 					break;
-			}
+			} // end switch
 
-		}
+		}// end while
+		*pos = 0;
 	}
-
+	if(dsfp)
+		fclose(dsfp);
 	return 0;
 }
 
@@ -338,7 +376,9 @@ int settings(const char *argv[]){
 		return -1;
 	}
 	*/
-	dynamicstrinput(strpath);
+	if (dynamicstrinput(strpath) < 0){
+		return -1;
+	}
 
 	if(argv[3]){
 		strncpy(configpath, argv[3], 64);
--- p1/Main.c
+++ p1/Main.c
@@ -7,7 +7,7 @@ int main(int argc, const char *argv[])
 	if(settings(argv)){
 		return -1;
 	}
-//	align(s1,s2);
+	align(s1,s2);
 
 //	if(local)
 //		localretrace(highscore[0],highscore[1]);
