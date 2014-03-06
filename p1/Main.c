13cf97c537ee410bde552d9860f9e3e733f09334
Bryce Handerson <bryce.handerson@gmail.com>
2014-03-06 10:48:44 -0800

    uncommented print alignment

--- p1/Main.c
+++ p1/Main.c
@@ -10,10 +10,10 @@ int main(int argc, const char *argv[])
 	//printf("S1 length: %d\nS2 length: %d\n", (int)strlen(s1), (int)strlen(s2));
 	align(s1,s2);
 	//printarray();
-//	if(local)
-//		localretrace(highscore[0],highscore[1]);
-//	else
-//		dynamicretrace();
+	if(local)
+		localretrace(highscore[0],highscore[1]);
+	else
+		dynamicretrace();
 	dynamicfree();
 
 	return 0;
