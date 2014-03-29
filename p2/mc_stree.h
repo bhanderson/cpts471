9cb230af5cb9e2742dd696ed931bdbcec622fb58
Andrew M Bates <andrewbates09@gmail.com>
2014-03-29 11:38:08 -0700

    just waiting for tires

---
+++ p2/mc_stree.h
@@ -0,0 +1,24 @@
+/*
+ * Author:      Bryce Handerson (bhanderson)
+ *              Andrew M Bates (abates09 | andrewbates09)
+ * File:        mc_stree.h
+ * Project:     CptS 471 - Project 2
+ * Due:         April 04, 2014
+ * 
+ * Notes:
+ * The goal of this project is to implement the McCreight's suffix tree con-
+ *      struction algorithm, given an input string s (in the FASTA file format)
+ *      and a file containing the input alphabet.
+ */
+
+
+/* ---------- INCLUDES ---------- */
+
+
+/* ---------- DEFINITIONS ------- */
+
+
+/* ---------- PROTOTYPES -------- */
+
+
+int main (int argc, char* argv[]);
